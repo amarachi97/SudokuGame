94a69a828e77fb1e1aaec6feb8ed2733b9979f2d
amarachi97 <amara_iwuh@hotmail.com>
2020-07-01 17:11:49 -0500

    added comments and accounted for invalid input

--- sudoku/board.py
+++ sudoku/board.py
@@ -53,7 +53,12 @@ HARD = [[4,0,0,0,0,0,0,0,0],
 game = EASY
 game_played = np.copy(game)
 class Game:
+    """
+    __init__(self)
     
+    Description: sets the width, height and the space at the bottom for time displayed.
+                 This also sets the screen
+    """
     def __init__(self):
         pygame.display.set_caption("Sudoku")
         self.width = 540
@@ -83,7 +88,13 @@ class Game:
             game = make.create()
             
         game_played = np.copy(game)
-        
+    
+    
+    """
+    start_game(self)
+    
+    Description: this function starts the game and handles all the events that occur during the game
+    """
     def start_game(self):
         global RED
         global BLACK
@@ -127,9 +138,11 @@ class Game:
                         elif event.key == pygame.K_RETURN:
                             row = int (pos[1]/(self.width//9))
                             col = int (pos[0]/(self.height//9))
-                            if s.checkPossible(row, col, int(text), game_played):
-                                game_played[row][col] = int (text)
-                                text = ""
+                            entry = int (text)
+                            if entry > 0 and entry < 10:
+                                if s.checkPossible(row, col, entry, game_played):
+                                    game_played[row][col] = entry
+                                    text = ""
                         elif event.key == pygame.K_d:
                             row = int (pos[1]/(self.width//9))
                             col = int (pos[0]/(self.height//9))
@@ -149,17 +162,19 @@ class Game:
                     currentTime = "00:00"
                 else:
                     currentTime = self.get_time()
-                 
+                
+                #render the computer solved time
                 solvedtimeImg = font.render(solvedtimeText, True, BLACK)
                 solvedtime_rect = solvedtimeImg.get_rect()
                 solvedtime_rect.bottomright = (self.width-40, self.height+self.space - 5)
-                    
+                 
+                #render the timer
                 timerImg = font.render(currentTime, True, BLACK)
                 timer_rect = timerImg.get_rect()
                 timer_rect.bottomleft = (5, self.height+self.space - 5)
             
                 
-                
+                #render the entered text
                 textImg = font.render(text, True, RED)
                 rect.size = textImg.get_size()
                 cursor.topleft= rect.topright
@@ -183,13 +198,17 @@ class Game:
                     
         pygame.display.quit()   
         
-            
+    """
+    draw_board(self)
+    
+    Description: draws the board during the course of the execution
+    
+    """
     def draw_board(self):
         global BLACK
         #horizontal lines
         line_count = 0
         for y in range(0, self.width+1, self.width//9):
-            #++line_count
             if line_count % 3 == 0 and line_count != 0:
                 pygame.draw.line(self.screen, BLACK, (0, y), (self.width, y), 2)
             pygame.draw.line(self.screen, BLACK, (0, y), (self.width, y))
@@ -204,7 +223,12 @@ class Game:
             line_count += 1
             
         
-        
+    """
+    insert_nums(self)
+    
+    Description: inserts the numbers in the empty space when entered by the user
+    
+    """    
     def insert_nums(self):
         global game
         global game_played
@@ -223,6 +247,14 @@ class Game:
                     img = font.render(str(game_played[row][col]), True, BLUE)
                     self.screen.blit(img, (x,y))
 
+
+    """
+    get_time(self)
+    
+    Description: updates the clock/timer at the bottom of the window
+    
+    returns the calculated time
+    """
     def get_time(self):
            timer[3] += 1
            timer[0] = math.floor((timer[3] / 100) / 60);
@@ -231,13 +263,20 @@ class Game:
            
            return self.leadingZero(timer[0]) + ":" + self.leadingZero(timer[1])
        
+    """
+    leadingZero(self)
+    
+    Description: adds a leading zero to the time displayed
+    
+    """
     def leadingZero(self, num):
         if num < 10:
             return "0" + str(num)
         return str(num)
            
-           
 
+        
+#MAIN
 start = Game()
 start.pick_game()
 start.start_game()
